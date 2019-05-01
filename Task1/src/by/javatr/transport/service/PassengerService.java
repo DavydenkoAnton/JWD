399ad6a65f8540f34a1d31e50d60682cea34bbfe
anton <minskdevelopment@gmail.com>
2019-05-01 22:42:15 +0300

    maven added

--- Task1/src/by/javatr/transport/service/PassengerService.java
+++
@@ -1,6 +0,0 @@
-package by.javatr.transport.service;
-
-public interface PassengerService {
-    void setName(String name);
-    void setBaggageCount(int baggageCount);
-}
--- Task1/src/by/javatr/transport/service/TrainPassengerService.java
+++
@@ -1,9 +0,0 @@
-package by.javatr.transport.service;
-
-import by.javatr.transport.entity.TrainCarPassenger;
-
-public interface TrainPassengerService {
-    void addTrainCarPassenger(int id);
-
-    void getTrainCarPassenger();
-}
