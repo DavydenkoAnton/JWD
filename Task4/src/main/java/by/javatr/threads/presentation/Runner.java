10bda0e40622508c1c533475a8951f660d69242f
anton <minskdevelopment@gmail.com>
2019-06-11 13:57:42 +0300

    update Task4

--- Task4/src/main/java/by/javatr/threads/presentation/Runner.java
+++ Task4/src/main/java/by/javatr/threads/presentation/Runner.java
@@ -1,7 +1,13 @@
 package by.javatr.threads.presentation;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Runner {
+    private static final Logger LOG = LogManager.getLogger(Runner.class);
     public static void main(String[] args) {
-        System.out.println();
+
+
+
     }
 }
