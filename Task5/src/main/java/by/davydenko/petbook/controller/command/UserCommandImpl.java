ff290b7b35d880988b951dbe87c320b419a8d32c
anton <minskdevelopment@gmail.com>
2019-07-12 22:49:54 +0300

    add UserCommandImpl.java

---
+++ Task5/src/main/java/by/davydenko/petbook/controller/command/UserCommandImpl.java
@@ -0,0 +1,12 @@
+package by.davydenko.petbook.controller.command;
+
+import by.davydenko.petbook.entity.User;
+
+import java.util.List;
+
+public class UserCommandImpl implements UserCommand {
+    @Override
+    public List<User> getUsers(String request) {
+        return null;
+    }
+}
