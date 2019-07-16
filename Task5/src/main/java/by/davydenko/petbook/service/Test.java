1afef296534b1e55a2c808dd6cb4bb6f3f8463f0
anton <minskdevelopment@gmail.com>
2019-07-16 13:59:05 +0300

    delete logs

---
+++ Task5/src/main/java/by/davydenko/petbook/service/Test.java
@@ -0,0 +1,13 @@
+package by.davydenko.petbook.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Test {
+
+    private static Logger logger= LogManager.getLogger(Test.class);
+    public static void main(String[] args) {
+        logger.info("test");
+    }
+
+}
